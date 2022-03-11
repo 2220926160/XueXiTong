35a2a4c6488fb79b5f5f291b02ab86a0a23cdcc7
2220926160 <73678222+2220926160@users.noreply.github.com>
2022-03-11 16:54:12 +0800

    2020-3-11

---
+++ HomeWork1/Code/Question5/.history/largest_factor_20220311165107.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    i = n - 1s
+    while i > 1 and n % i != 0:
+        i -= 1
+    if(i > 1):
+        return i
+    else:
+        return 1
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
