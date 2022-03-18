ad6892205c381253ad2349ef9944954ef946d0bd
2220926160 <73678222+2220926160@users.noreply.github.com>
2022-03-18 20:15:38 +0800

    2022-3-18

---
+++ HomeWork2/lab01/parsons_probs/.history/falling_20220318200223.py
@@ -0,0 +1,18 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    result = 1
+    for i in range(k, n) :
+        result *= i
+        i += 1
+    return result
